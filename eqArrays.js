1f9536802d42ffb1e8843af95d20bb2818d6fe23
robotsch <christianhumble22@gmail.com>
2022-03-08 14:34:44 -0500

    add eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  // Catch length mismatch
+  if (arr1.length === arr2.length) {
+    // Loop through elements
+    for (let i = 0; i < arr1.length; i += 1) {
+      // Return false on catching difference
+      if (!(arr1[i] === arr2[i])) {
+        return false;
+      }
+    }
+  } else {
+    // Return false on length mismatch
+    return false;
+  }
+  // Will only run after diff catches fail
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
