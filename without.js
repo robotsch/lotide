b7f7df4fdfe0c547286260e63462190aa3e4cb33
robotsch <christianhumble22@gmail.com>
2022-03-08 14:46:25 -0500

    add without.js

---
+++ without.js
@@ -0,0 +1,18 @@
+const without = function(srcArr, removeArr) {
+  // Initialize new array
+  let newArr = []
+  // Loop through source array
+  for(let element of srcArr) {
+    // If remove array doesn't include the current element of source array,
+    if(!(removeArr.includes(element))) {
+      // Add that element to the new array
+      newArr.push(element)
+    }
+  }
+  // Return new array
+  return newArr
+}
+
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
