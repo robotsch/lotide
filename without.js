06bf5b8dafbc6a0e161c9cab896d593bd531d051
robotsch <christianhumble22@gmail.com>
2022-03-08 14:47:27 -0500

    lint without.js

--- without.js
+++ without.js
@@ -1,18 +1,18 @@
 const without = function(srcArr, removeArr) {
   // Initialize new array
-  let newArr = []
+  let newArr = [];
   // Loop through source array
-  for(let element of srcArr) {
+  for (let element of srcArr) {
     // If remove array doesn't include the current element of source array,
-    if(!(removeArr.includes(element))) {
+    if (!(removeArr.includes(element))) {
       // Add that element to the new array
-      newArr.push(element)
+      newArr.push(element);
     }
   }
   // Return new array
-  return newArr
-}
+  return newArr;
+};
 
 
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
