9d235dc16e4b0972646f6b79e05cf45040bca7de
robotsch <christianhumble22@gmail.com>
2022-03-09 16:19:46 -0500

    add letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,17 @@
+const letterPositions = function(sentence) {
+  const results = {}
+  // Remove whitespace, thank you StackOverflow
+  let workingStr = sentence.replace(/\s+/g, '')
+  // Loop through each character
+  for(let i = 0; i < workingStr.length; i += 1) {
+    if (results[workingStr[i]]) {
+      results[workingStr[i]].push(String(i))
+    } else {
+      results[workingStr[i]] = [String(i)]
+    }
+
+  }
+  return results
+};
+
+console.log(letterPositions("hello"))
