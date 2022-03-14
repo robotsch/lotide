191f75d4bd5011eb04c9b47d0f9a8e1196ebd810
robotsch <christianhumble22@gmail.com>
2022-03-14 11:26:03 -0400

    refactor assertArraysEqual, add test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  // Catch length mismatch
-  if (arr1.length === arr2.length) {
-    // Loop through elements
-    for (let i = 0; i < arr1.length; i += 1) {
-      // Return false on catching difference
-      if (!(arr1[i] === arr2[i])) {
-        return false;
-      }
-    }
-  } else {
-    // Return false on length mismatch
-    return false;
-  }
-  // Will only run after diff catches fail
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -24,5 +8,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+module.exports = assertArraysEqual
